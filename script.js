f3a6ab5207e2dbefa9fc8ff4556a08cefe2452f4
Djimon <woofless77@gmail.com>
2021-12-01 08:48:01 +0800

    added cookies

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Constants
+var NUM = "numTasks";
 // make item checked when clicked on
 var tasks = document.querySelector('ul');
 tasks.addEventListener('click', function(ev) {
@@ -5,11 +7,15 @@ tasks.addEventListener('click', function(ev) {
     ev.target.classList.toggle('checked');
   }
 }, false);
+
 // Add new task when clicking the + button
 function newTask() {
+  // update cookies
+
   var li = document.createElement("li");
   var name = document.getElementById("taskName").value;
   var txt = document.createTextNode(name);
+  txt.setAttribute("id","itemName");
   li.appendChild(txt);
   document.getElementById("taskList").appendChild(li);
 
@@ -27,6 +33,60 @@ function newTask() {
     finish[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
+      var j;
+      var found = false;
+      for (j=0;j<Number(get_cookie(NUM));j++) {
+        if (get_cookie(j)==this.getElementById("itemName").value) {
+          found = true;
+        }
+        if (found) {
+          set_cookie(j,get_cookie(j+1));
+        }
+      }
+      set_cookie(NUM,Number(get_cookie(NUM))-1);
     }
   }
+
+  set_cookie(NUM,Number(get_cookie(NUM))+1);
+  set_cookie(Number(get_cookie(NUM)),name);
 }
+
+// Cookies
+function set_cookie(cname, cvalue) {
+  var exdays = 365;
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function get_cookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function check_cookie(x, backup) {
+  if (get_cookie(x) == "") {
+    set_cookie(x, backup, 365);
+  }
+}
+
+function initialise() {
+  check_cookie(NUM,0);
+  var i;
+  for (i=0;i<Number(get_cookie(NUM));i++) {
+
+  }
+}
+
+initialise();
